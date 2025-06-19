3395204a3b77912d6d5a7e0cd3ccc4905264a519
bongnteh-romarick-ndzelen <ndzelenromarick@gmail.com>
2025-06-19 09:51:31 -0400

    Configure for Render deployment

--- Suscam/build.sh
+++ Suscam/build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# Exit on errors
+set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Run migrations
 python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
